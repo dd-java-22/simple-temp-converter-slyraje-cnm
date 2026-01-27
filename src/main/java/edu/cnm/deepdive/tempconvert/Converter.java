2f626e102f87eb9111a0604f518717da1811eb07
Michael Sylvester <msylvester@cnm.edu>
2026-01-27 14:18:40 -0700

    Starting tests for converting Fahrenheit to Celsius.

--- src/main/java/edu/cnm/deepdive/tempconvert/Converter.java
+++ src/main/java/edu/cnm/deepdive/tempconvert/Converter.java
@@ -3,13 +3,17 @@ package edu.cnm.deepdive.tempconvert;
 public class Converter {
 
   public static final double CELSIUS_ABSOLUTE_ZERO = -273.15;
-  public static final double CELSIUS_TO_FAHRENHEIT_SCALE = 9.0 / 5;
-  public static final double CELSIUS_TO_FAHRENHEIT_OFFSET = 32;
+  private static final double CELSIUS_TO_FAHRENHEIT_SCALE = 9.0 / 5;
+  private static final int CELSIUS_TO_FAHRENHEIT_OFFSET = 32;
 
   public double convertC2F(double celsius) {
     if (celsius < CELSIUS_ABSOLUTE_ZERO) {
       throw new IllegalArgumentException();
     }
     return celsius * CELSIUS_TO_FAHRENHEIT_SCALE + CELSIUS_TO_FAHRENHEIT_OFFSET;
+  }
+
+  public double convertF2C(double fahrenheit) {
+    throw new UnsupportedOperationException("Not yet implemented! Be patient!");
   }
 }
--- src/test/java/edu/cnm/deepdive/tempconvert/ConverterTest.java
+++ src/test/java/edu/cnm/deepdive/tempconvert/ConverterTest.java
@@ -1,8 +1,8 @@
 package edu.cnm.deepdive.tempconvert;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -13,7 +13,7 @@ class ConverterTest {
   static final double MIN_ABSOLUTE_TOLERANCE = 0.0000000001;
 
   @ParameterizedTest
-  @CsvFileSource(resources = "convert-c2f-valid.csv", useHeadersInDisplayName = true)
+  @CsvFileSource(resources = "convert-valid.csv", useHeadersInDisplayName = true)
   void convertC2F_valid(double celsius, double expected) {
     double tolerance = Math.max(MIN_ABSOLUTE_TOLERANCE, Math.abs(expected * TOLERANCE_SCALE));
     assertEquals(
@@ -21,13 +21,10 @@ class ConverterTest {
         new Converter().convertC2F(celsius),
         tolerance
     );
-
-
-
   }
 
   @ParameterizedTest
-  @ValueSource(doubles = {-273.16, -Double.MAX_VALUE})
+  @ValueSource(doubles = {Converter.CELSIUS_ABSOLUTE_ZERO - 0.01, -Double.MAX_VALUE})
   void convertC2F_invalid(double celsius) {
     try {
       new Converter().convertC2F(celsius);
@@ -37,5 +34,19 @@ class ConverterTest {
     } catch (Exception e) {
       fail("Illegal argument exception expected, but %s thrown.".formatted(e.getClass().getSimpleName()));
     }
+  }
+
+  @ParameterizedTest
+  @ValueSource(doubles = {-459.67, -Double.MAX_VALUE})
+  void convertF2C_invalid(double fahrenheit) {
+    try {
+      new Converter().convertF2C(fahrenheit);
+      fail("Illegal argument exception expected, but no exception thrown.");
+    } catch (IllegalArgumentException e) {
+//      Do nothing: This exception is expected.
+    } catch (Exception e) {
+      fail("Illegal argument exception expected, but %s thrown.".formatted(e.getClass().getSimpleName()));
+    }
+
   }
 }

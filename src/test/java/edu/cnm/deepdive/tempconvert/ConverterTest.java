313a988f6f6356bbc4aac2e02564f59dc9973016
Michael Sylvester <msylvester@cnm.edu>
2026-01-27 13:53:58 -0700

    Starting test for invalid inputs.

--- src/test/java/edu/cnm/deepdive/tempconvert/ConverterTest.java
+++ src/test/java/edu/cnm/deepdive/tempconvert/ConverterTest.java
@@ -14,8 +14,8 @@ class ConverterTest {
 
   @ParameterizedTest
   @CsvFileSource(resources = "convert-c2f-valid.csv", useHeadersInDisplayName = true)
-  void convertC2F(double celsius, double expected) {
-    double tolerance = Math.max(Math.abs(expected * TOLERANCE_SCALE), MIN_ABSOLUTE_TOLERANCE);
+  void convertC2F_valid(double celsius, double expected) {
+    double tolerance = Math.max(MIN_ABSOLUTE_TOLERANCE, Math.abs(expected * TOLERANCE_SCALE));
     assertEquals(
         expected,
         new Converter().convertC2F(celsius),
@@ -29,6 +29,13 @@ class ConverterTest {
   @ParameterizedTest
   @ValueSource(doubles = {-273.16, -Double.MAX_VALUE})
   void convertC2F_invalid(double celsius) {
-    double actual = new Converter().convertC2F(celsius);
+    try {
+      new Converter().convertC2F(celsius);
+      fail("Illegal argument exception expected, but no exception thrown.");
+    } catch (IllegalArgumentException e) {
+//      Do nothing: This exception is expected.
+    } catch (Exception e) {
+      fail("Illegal argument exception expected, but %s thrown.".formatted(e.getClass().getSimpleName()));
+    }
   }
 }

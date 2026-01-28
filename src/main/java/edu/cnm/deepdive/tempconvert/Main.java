077d584bae32f05586b5c4608be60efe8f1cd77d
Michael Sylvester <msylvester@cnm.edu>
2026-01-27 17:05:00 -0700

    Completed input handler with scanner.

--- src/main/java/edu/cnm/deepdive/tempconvert/Main.java
+++ src/main/java/edu/cnm/deepdive/tempconvert/Main.java
@@ -1,9 +1,11 @@
 package edu.cnm.deepdive.tempconvert;
 
+import edu.cnm.deepdive.tempconvert.controller.InputHandler;
+
 public class Main {
 
   public static void main(String[] args) {
-//    TODO: 2026-01-27 Create InputHandler and let it run the show.
+    new InputHandler().process();
 
   }
 
--- src/main/java/edu/cnm/deepdive/tempconvert/controller/InputHandler.java
+++ src/main/java/edu/cnm/deepdive/tempconvert/controller/InputHandler.java
@@ -1,10 +1,44 @@
 package edu.cnm.deepdive.tempconvert.controller;
 
+import edu.cnm.deepdive.tempconvert.model.InputMode;
+import edu.cnm.deepdive.tempconvert.service.Converter;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /**
  * Handles user input from stdin, using a converter instance to perform conversions with InputMode to select conversions and writing output to stdout.
  */
 public class InputHandler {
 
+private static final Pattern NUMBER_PATTERN = Pattern.compile("[-+]?\\d+(?:\\.\\d*)?");
+private static final String CELSIUS_INPUT_PATTERN = "Celsius = %.2f; Fahrenheit = %.2f%n";
+private static final String FAHRENHEIT_INPUT_PATTERN = "Fahrenheit = %.2f; Celsius = %.2f%n";
 
+private InputMode mode = InputMode.CELSIUS;
+
+public void process() {
+  Converter converter = new Converter();
+  Scanner scanner = new Scanner(System.in);
+  while (scanner.hasNext()) {
+    if (scanner.hasNext(NUMBER_PATTERN)) {
+      String input = scanner.next(NUMBER_PATTERN);
+      double inputValue = Double.parseDouble(input);
+      String output = switch (mode) {
+        case CELSIUS -> {
+          double fahrenheit = converter.convertC2F(inputValue);
+          yield CELSIUS_INPUT_PATTERN.formatted(inputValue, fahrenheit);
+        }
+        case FAHRENHEIT -> {
+          double celsius = converter.convertF2C(inputValue);
+          yield FAHRENHEIT_INPUT_PATTERN.formatted(inputValue, celsius);
+        }
+      };
+      System.out.print(output);
+    } else {
+      mode = InputMode.valueOf(scanner.next().strip().toUpperCase());
+
+    }
+  }
+}
 
 }
